d5b684178aee9d6bd064425514b395f385de450c
= <=>
2022-09-26 23:45:41 +0300

    .

--- src/main/java/com/example/chat/controllers/MessageController.java
+++ src/main/java/com/example/chat/controllers/MessageController.java
@@ -1,11 +1,13 @@
 package com.example.chat.controllers;
 
 
+import com.example.chat.models.Message;
 import com.example.chat.services.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,11 +20,9 @@ public class MessageController {
         return "chat";
     }
 
-//    @PostMapping("/new")
-//    public String createMessage(Message message) {
-//        messageService.save(message);
-//        return "redirect:/";
-//    }
-
-
+    @PostMapping("/new")
+    public String createMessage(Message message) {
+        messageService.save(message.getUserName(), message.getMessage());
+        return "redirect:/";
+    }
 }
